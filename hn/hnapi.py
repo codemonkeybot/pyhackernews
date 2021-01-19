ca101b4750d5e8a3267465651edae4efff575bd9
codemonkeybot <codemonkeybot@users.noreply.github.com>
2021-01-19 13:32:36 -0700

    Update for Python3

--- hn/hnapi.py
+++ hn/hnapi.py
@@ -1,6 +1,7 @@
 import itertools
 from bs4 import BeautifulSoup
-from urllib2 import Request, urlopen, URLError
+from urllib.request import Request, urlopen
+from urllib.error import URLError
 
 BASE_URL = 'http://news.ycombinator.com'
 
@@ -35,11 +36,11 @@ class HN():
             response = urlopen(req).read()
         except URLError as e:
             if hasattr(e, 'reason'):
-                print 'We failed to reach a server.'
-                print 'Reason: ', e.reason
+                print('We failed to reach a server.')
+                print('Reason: ', e.reason)
             elif hasattr(e, 'code'):
-                print 'The server couldn\'t fulfill the request.'
-                print 'Error code: ', e.code
+                print('The server couldn\'t fulfill the request.')
+                print('Error code: ', e.code)
         soup = BeautifulSoup(response)
         return soup
 
